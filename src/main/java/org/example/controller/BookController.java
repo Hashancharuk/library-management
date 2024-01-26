cf8299520c0532fdbf757e776223371d1b713699
HashanCharuk <charukhashan@gmail.com>
2024-01-26 18:52:54 +0530

    Add Delete Mapping

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.dto.Book;
 import org.example.entity.BookEntity;
 import org.example.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +21,12 @@ public class BookController {
         service.addBook(book);
     }
     @GetMapping
-    public Iterable<BookEntity> getBooks(){
-        return service.getBooks();
+    public Iterable<BookEntity> getBooks(){return service.getBooks();}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteBook(@PathVariable Long id){
+        return service.deleteBook(id) ?
+             ResponseEntity.ok("Deleted"):
+             ResponseEntity.notFound().build();
     }
 }
